b74287a0607d102e39546e9e4db73711434d760d
alanjheyson <alan.costa@professor.edu.pi.gov.br>
2025-06-06 08:00:59 -0300

    Create alunoClass.php

---
+++ alunoClass.php
@@ -0,0 +1,21 @@
+<?php
+
+class Aluno{
+  private $id;
+  private $nome;
+  private $matricula;
+  private $serie;
+  private $turma;
+  private $turno;
+  private $sexo;
+  private $dataMascimento;
+  private $pai;
+  private $mae;
+
+
+
+}
+
+
+
+?>
